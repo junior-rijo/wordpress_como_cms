6c6019c129bb882bf5f3a2f8f370934a9d359a68
Your Name <you@example.com>
2021-07-23 16:43:49 -0300

    add_arquivos

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="http://foodtech.test/">Menu</a></li>
+					<li><a href="http://foodtech.test/sobre/">Sobre</a></li>
+					<li><a href="http://foodtech.test/contato/">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p><?php bloginfo('name'); ?>© <?php echo date("Y") ?>. Alguns direitos reservados.</p>
+		</footer>
+        <?php wp_footer(); ?>
+	</body>
+</html>
